c93f679abf5a6250fabd3f39f25a450be34d3beb
Adarsh Sharma <a.sharma30double@gmail.com>
2023-09-28 11:42:53 +0530

    origin points to ec2 instance http

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: ["https://colortrapgame.com", "https://colourtrapgame.com"],
+    origin: ["http://ec2-3-110-118-192.ap-south-1.compute.amazonaws.com"],
     allowedHeaders: ["Authorization"],
   },
 });
